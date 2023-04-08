b6f6af7865cbc433a769022f27db615e7989fec7
Anantha-Kandrapu <akandrap@asu.edu>
2023-04-07 23:45:49 -0700

    Generator Complete

--- generator.py
+++ generator.py
@@ -7,8 +7,9 @@ import random
 from torch.utils.tensorboard import SummaryWriter
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-file = unidecode.unidecode(open("../textdata/names.txt").read())
-all_characters = string.__all__()
+file = unidecode.unidecode(open("./textdata/names.txt").read())
+all_characters = string.printable
+n_characters = len(all_characters)
 
 
 class RNN(nn.Module):
@@ -17,7 +18,7 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.embed = nn.Embedding(input_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batchFirst=True)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x, hidden, cell):
@@ -29,7 +30,7 @@ class RNN(nn.Module):
     def init_hidden(self, batch_size):
         hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
         cell = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
-        print("f(Hidden Size {hidden.shape})")
+        # print("f(Hidden Size {hidden.shape})")
         return hidden, cell
 
 
@@ -45,7 +46,7 @@ class Generator:
 
     def char_tensor(self, string):
         tensor = torch.zeros(len(string)).long()
-        print("f(char_tensor : {tensor})")
+        # print("f(char_tensor : {tensor})")
         for c_idx in range(len(string)):
             tensor[c_idx] = all_characters.index(string[c_idx])
         return tensor
@@ -59,11 +60,28 @@ class Generator:
         for i in range(self.batch_size):
             text_input[i, :] = self.char_tensor(text_str[:-1])
             text_target[i, :] = self.char_tensor(text_str[1:])
-            print("f(text_input : {text_input} text_target :{text_target})")
+            # print("f(text_input : {text_input} text_target :{text_target})")
         return text_input.long(), text_target.long()
 
-    def generate(self):
-        pass
+    def generate(self, initial_str="A", predict_len=100, temperature=0.85):
+        hidden, cell = self.rnn.init_hidden(batch_size=self.batch_size)
+        initial_input = self.char_tensor(initial_str)
+        predicted = initial_str
+        for p in range(len(initial_str) - 1):
+            _, (hidden, cell) = self.rnn(
+                initial_input[p].view(1).to(device), hidden, cell
+            )
+        last_char = initial_input[-1]
+        for p in range(predict_len):
+            output, (hidden, cell) = self.rnn(
+                last_char.view(1).to(device), hidden, cell
+            )
+            output_dist = output.data.view(-1).div(temperature).exp()
+            top_char = torch.multinomial(output_dist, 1)[0]
+            predicted_char = all_characters[top_char]
+            predicted += predicted_char
+            last_char = self.char_tensor(predicted_char)
+        return predicted
 
     def train(self):
         self.rnn = RNN(
@@ -72,4 +90,28 @@ class Generator:
         optimizer = torch.optim.Adam(self.rnn.parameters(), lr=self.lr)
         criterion = nn.CrossEntropyLoss()
         writer = SummaryWriter(f"runs/names0")
-        print('START TRAINING')
+        print("START TRAINING")
+
+        for epoch in range(1, self.num_epochs + 1):
+            inp, target = self.get_random_batch()
+            hidden, cell = self.rnn.init_hidden(batch_size=self.batch_size)
+            self.rnn.zero_grad()
+            loss = 0
+            inp = inp.to(device)
+            target = target.to(device)
+            for c in range(self.chunk_len):
+                output, (hidden, cell) = self.rnn(inp[:, c], hidden, cell)
+                loss += criterion(output, target[:, c])
+            loss.backward()
+            optimizer.step()
+            loss = loss.item() / self.chunk_len
+
+            if epoch % self.print_every == 0:
+                print(self.generate())
+                print("f(loss : {loss})")
+
+            writer.add_scalar("Training loss", loss, global_step=epoch)
+
+
+gennames = Generator()
+gennames.train()
